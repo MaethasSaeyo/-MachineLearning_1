f8a77eb3055599017befd15945215381e49be1b2
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-09-28 18:02:21 +0700

    9/28/2023_5:59_V.0.0.7(model min-max temp)

--- ML1.py
+++ ML1.py
@@ -1,31 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-#y = ax+c
-rng = (np.random)
-#การจำลองข้อมูล
-x = rng.rand(50)*10
-c = rng.randn(50)
-y = 2*x+c
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-#linear regression model
+dataset = pd.read_csv("Weather.csv")
+
+#train & test set
+x = dataset["MinTemp"].values.reshape(-1,1)
+y = dataset["MaxTemp"].values.reshape(-1,1)
+
+# 80% - 20%
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+
+#trainning
 model = LinearRegression()
+model.fit(x_train,y_train)
+#test
+y_pred =  model.predict(x_test) 
+
+#plt.scatter(x_test,y_test)
+#plt.plot(x_test,y_pred,color="red",linewidth=2)
+#plt.show()
 
 
-x_new = x.reshape(-1,1)
+#compare true data & predict data
+df = pd.DataFrame({'Actually':y_test.flatten(),'Predicted':y_pred.flatten()})
 
-#train
-model.fit(x_new,y)
-
-#test model
-xfit = np.linspace(-1,11)
-xfit_new = xfit.reshape(-1,1)
-
-yfit = model.predict(xfit_new)
-
-
-# analysis model &result
-plt.scatter(x,y)
-plt.plot(xfit,yfit)
+df1 = df.head(20)
+df1.plot(kind="bar",figsize=(16,10))
 plt.show()
-
