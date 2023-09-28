4b5e5476cc8117019d20cf38e9971098489f620f
MaethasSaeyo <130046454+MaethasSaeyo@users.noreply.github.com>
2023-09-28 13:43:49 +0700

    9/28//2023_1:42_V.0.0.4(seadorn)

--- ML1.py
+++ ML1.py
@@ -1,22 +1,7 @@
-from scipy.io import loadmat
+import seaborn as sb
 import matplotlib.pyplot as plt
+iris_dataset = sb.load_dataset('iris')
 
-mnist_raw = loadmat("mnist-original.mat")
-
-mnist ={
-    "data":mnist_raw["data"].T,
-    "target":mnist_raw["label"][0]
-}
-x = mnist["data"]
-y = mnist["target"]
-
-number = x[2000]
-num_img = number.reshape(28,28)
-
-print(int(y [2000]))
-plt.imshow(
-    num_img,
-    cmap=plt.cm.binary,
-    interpolation="nearest"
-    )
+sb.set()
+sb.pairplot(iris_dataset,hue='species',size=2)
 plt.show()
